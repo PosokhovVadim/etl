d5521ca7ac38bef98cbc29a5cf3c3313607f8d7a
PosokhovVadim <posokhovvadyusha@gmail.com>
2025-03-17 00:47:18 +0400

    add full replication

--- dags/mongo_to_dag.py
+++ dags/mongo_to_dag.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 import psycopg2
+import json
 
-def replicate_data():
+def replicate_data():    
     client = MongoClient("mongodb://mongo_db:27017/")
     db = client["etl_database"]
+
     conn = psycopg2.connect(
         dbname="airflow_db",
         user="airflow",
@@ -11,16 +13,31 @@ def replicate_data():
         host="postgres",
     )
     cursor = conn.cursor()
-    
-    cursor.execute("TRUNCATE TABLE user_sessions")  
 
-    for session in db["UserSessions"].find():
-        cursor.execute(
-            "INSERT INTO user_sessions (session_id, user_id, start_time, end_time, device) VALUES (%s, %s, %s, %s, %s)",
-            (session["session_id"], session["user_id"], session["start_time"], session["end_time"], session["device"]),
-        )
+    tables = {
+        "UserSessions": "user_sessions",
+        "ProductPriceHistory": "product_price_history",
+        "EventLogs": "event_logs",
+        "SupportTickets": "support_tickets",
+        "UserRecommendations": "user_recommendations",
+        "ModerationQueue": "moderation_queue",
+        "SearchQueries": "search_queries"
+    }
 
-    conn.commit()
+    for collection, table in tables.items():
+        print(f"🔄 Репликация {collection} → {table}")
+
+        cursor.execute(f"TRUNCATE TABLE {table} RESTART IDENTITY;")
+        for doc in db[collection].find():
+            columns = ", ".join(doc.keys())
+            values = [json.dumps(v) if isinstance(v, (dict, list)) else v for v in doc.values()]
+            placeholders = ", ".join(["%s"] * len(values))
+
+            insert_query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
+            cursor.execute(insert_query, values)
+
+        conn.commit()
+        print(f"Replication {collection} has done!")
+
     cursor.close()
     conn.close()
-
